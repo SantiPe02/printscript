59e1aacd8cd7e6d73edc3d5ead842061b6cc6b8a
Franco B <franco.barravecchia2002@gmail.com>
2024-03-28 13:40:33 -0300

    update: remove from src

--- src/main/kotlin/formater/IFormatter.kt
+++
@@ -1,8 +0,0 @@
-package formater
-
-interface IFormatter {
-    /**
-     * Given some sourcecode it will return the same code with a standard format
-     */
-    fun formatString(sourceCode: String): String
-}
