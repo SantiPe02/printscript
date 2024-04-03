0df20a4440c3997cdb6453ae9140316d38278aa5
Franco B <franco.barravecchia2002@gmail.com>
2024-04-02 21:32:19 -0300

    fix: format null in method

--- formatter/src/main/kotlin/formater/format.kt
+++ formatter/src/main/kotlin/formater/format.kt
@@ -4,8 +4,8 @@ import formater.configurationReader.ConfigurationReaderProvider
 import java.io.File
 
 fun format(file : String, confifFile : File) : Result<Boolean> {
-    val reader = ConfigurationReaderProvider().getReader(confifFile.extension).getOrElse { return Result.failure(it.cause!!) }
-    val rules =translateFormatterConfigurationToRules(reader.readFileAndBuildRules(confifFile).getOrElse { return Result.failure(it.cause!!) })
+    val reader = ConfigurationReaderProvider().getReader(confifFile.extension).getOrElse { return Result.failure(it) }
+    val rules =translateFormatterConfigurationToRules(reader.readFileAndBuildRules(confifFile).getOrElse { return Result.failure(it) })
     FileFormatter(RegexFormatter(rules)).format(file)
     return Result.success(true)
 }
