4b43ed9be7f9974154b56301933654d46f04ea6a
Franco B <franco.barravecchia2002@gmail.com>
2024-04-07 20:10:06 -0300

    feat: validate command

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -3,12 +3,14 @@ import commands.Analyzing
 import commands.Execute
 import commands.Formatting
 import commands.Printscript
+import commands.Validate
 import interpreter.DefaultInterpreter
 import lexer.LexerImpl
 import parser.MyParser
 
 fun main(args: Array<String>): Unit =
     Printscript().subcommands(
+        Validate(LexerImpl(), MyParser()),
         Formatting(),
         Analyzing(LexerImpl(), MyParser(), MyLinter()),
         Execute(LexerImpl(), MyParser(), DefaultInterpreter()),
