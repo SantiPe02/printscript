46f919c7cc409cdd88ccc4078cb734e01fa1a665
Franco B. <franco.barravecchia2002@gmail.com>
2024-04-18 17:40:43 -0300

    Revert "update: execute use LargeFileReading"
    This reverts commit 48a4fc8f433746f6bc6ae5fd16f62cf389ba0845.

--- cli/src/main/kotlin/commands/Execute.kt
+++ cli/src/main/kotlin/commands/Execute.kt
@@ -1,15 +1,13 @@
 package commands
 
-import PartialStringReadingLexer
+import ast.Scope
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.CliktError
 import com.github.ajalt.clikt.parameters.arguments.argument
 import interpreter.Interpreter
 import lexer.Lexer
 import parser.Parser
-import java.io.BufferedInputStream
 import java.io.File
-import java.nio.charset.Charset
 
 class Execute(val lexer: Lexer, val parser: Parser, val interpreter: Interpreter) : CliktCommand() {
     val fileDir: String by argument()
@@ -18,7 +16,12 @@ class Execute(val lexer: Lexer, val parser: Parser, val interpreter: Interpreter
         val file = File(fileDir)
         if (!file.exists()) throw CliktError("The file to run was not found at $fileDir")
 
-        val report = interpretFile(file).report
+        val ast: Scope =
+            parser.parseTokens(lexer.tokenize(file.readText())).getOrElse {
+                throw CliktError(it.message)
+            }
+        interpreter.interpret(ast)
+        val report = interpreter.report
         report.outputs.forEach { out -> echo(out) }
 
         if (report.errors.isNotEmpty()) {
@@ -26,35 +29,5 @@ class Execute(val lexer: Lexer, val parser: Parser, val interpreter: Interpreter
                 "Found errors in while executing:\n" + report.errors.joinToString("\n") { it },
             )
         }
-    }
-
-    private fun interpretFile(file: File): Interpreter {
-        if (file.extension != "ps") {
-            throw CliktError("was expecting a ps file and got ${file.extension}")
-        }
-
-        file.inputStream().buffered().use { stream ->
-            return interpretStream(stream)
-        }
-    }
-
-    private fun interpretStream(stream: BufferedInputStream): Interpreter {
-        val buffer = ByteArray(1046)
-        var bytesRead = stream.read(buffer)
-        var tokenizer = PartialStringReadingLexer(lexer)
-        var interpreter = interpreter
-
-        while (bytesRead != -1) {
-            val textBlock = String(buffer, 0, bytesRead, Charset.defaultCharset())
-            val tokenizerAndTokens = tokenizer.tokenizeString(textBlock)
-            tokenizer = tokenizerAndTokens.first
-            val tokens = tokenizerAndTokens.second
-
-            val ast = parser.parseTokens(tokens).getOrElse { throw CliktError(it.message) }
-            interpreter = interpreter.interpret(ast)
-            bytesRead = stream.read(buffer)
-        }
-        stream.close()
-        return interpreter
     }
 }
