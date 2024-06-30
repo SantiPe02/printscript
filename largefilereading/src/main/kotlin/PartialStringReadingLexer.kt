4ee2b234db30cc5fe954e5f9261bf44154b4a615
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2024-06-30 00:07:04 -0300

    Making some changes on the PartialStringReadingLexer.kt

--- largefilereading/src/main/kotlin/PartialStringReadingLexer.kt
+++ largefilereading/src/main/kotlin/PartialStringReadingLexer.kt
@@ -5,19 +5,24 @@ import token.TokenInfo
 class PartialStringReadingLexer(
     val lexer: Lexer,
     val notProcessed: String = "",
-    val context: Context = Context(0),
+    val context: Context = Context(0), // Sería como un puntero, cuando lee "fer", el context terminará en 3 (o 2 nose)
 ) {
+    // en let ...; if()... el if no te lo tokeniza, porque está después del último ;
     fun tokenizeString(text: String): Pair<PartialStringReadingLexer, List<TokenInfo>> {
         var notProcessedAux = ""
         var toProcessText = notProcessed + text
         val auxContext: Context
 
         val lastColonIndex = toProcessText.lastIndexOf(';')
-        if (lastColonIndex != -1 && lastColonIndex != toProcessText.length - 1) {
-            notProcessedAux = toProcessText.substring(lastColonIndex + 1)
-            toProcessText = toProcessText.substring(0, lastColonIndex)
+        val lastBracketIndex = toProcessText.lastIndexOf('}')
+        val lastIndex = maxOf(lastColonIndex, lastBracketIndex)
+
+        if (lastIndex != -1 && lastIndex != toProcessText.length - 1) {
+            notProcessedAux = toProcessText.substring(lastIndex + 1)
+            toProcessText = toProcessText.substring(0, lastIndex + 1)
             auxContext = Context(context.startingLength + toProcessText.length + 1)
-        } else if (lastColonIndex == -1) {
+        } else if (lastIndex == -1) {
+            // if last index does not exist
             notProcessedAux = toProcessText
             toProcessText = ""
             auxContext = Context(context.startingLength)
